2c54367a811adada4e014e69bae4ad612e288820
Parsa Arababadi <arababadiparsa@gmail.com>
2024-09-30 23:13:36 +0330

    less code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
  
-import componentsImg from './assets/components.png';
-
+import { CORE_CONCEPTS } from './Data';
 
 
  const reactDscription =['Fundamental','Crucial','Core'];
@@ -20,12 +19,12 @@ import componentsImg from './assets/components.png';
     </header>
   );
 }
-function CoreConcept(props){
+function CoreConcept({image,title,description}){
   return(
 <li>
-  <img src={props.image} alt={props.title} />
-  <h3>{props.title}</h3>
-  <p>{props.description}</p>
+  <img src={image} alt={title} />
+  <h3>{title}</h3>
+  <p>{description}</p>
 </li>
   );
 }
@@ -37,13 +36,11 @@ function App() {
         <section id="core-concepts">     
              <h2>Core Consepts</h2>
              <ul>
-              <CoreConcept title="Components"
-               description="The core UI building block"
-               image={componentsImg}
+              <CoreConcept {...CORE_CONCEPTS[0]}
                />
-              <CoreConcept />
-              <CoreConcept />
-              <CoreConcept />
+              <CoreConcept {...CORE_CONCEPTS[1]} />
+              <CoreConcept {...CORE_CONCEPTS[2]} />
+              <CoreConcept {...CORE_CONCEPTS[3]} />
              </ul>
 </section>
       </main>
