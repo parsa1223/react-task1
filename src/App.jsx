dea1eae1954ab858f80d6da5b90d46bfdeafe222
Parsa Arababadi <arababadiparsa@gmail.com>
2024-10-07 23:31:48 +0330

    make them in each component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
- 
+ import Head from'./component/Header.jsx';
 import { CORE_CONCEPTS } from './Data';
+import CoreConcept from './component/CoreConsept.jsx';
 
+ 
+ 
 
- const reactDscription =['Fundamental','Crucial','Core'];
- function genRandomInt(max){
-  return Math.floor(Math.random() * (max+1));
- }
- function Head() {
-  const description = reactDscription[genRandomInt(2)];
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-function CoreConcept({image,title,description}){
-  return(
-<li>
-  <img src={image} alt={title} />
-  <h3>{title}</h3>
-  <p>{description}</p>
-</li>
-  );
-}
 function App() {
   return (
     <div>
---
+++ src/component/Header.jsx
@@ -0,0 +1,18 @@
+import reactImg from '../assets/react-core-concepts.png';
+const reactDscription =['Fundamental','Crucial','Core'];
+function genRandomInt(max){
+ return Math.floor(Math.random() * (max+1));
+}
+export default function Head() {
+    const description = reactDscription[genRandomInt(2)];
+    return (
+      <header>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    );
+  }
